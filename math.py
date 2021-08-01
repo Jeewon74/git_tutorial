f176762a1e8fc824e46d3d1f2f2b15f44b688235
Jeewon74 <jeewoncn@gmail.com>
2021-08-01 12:10:00 +0900

    changes made again

--- math.py
+++ math.py
@@ -3,10 +3,9 @@ def add(x,y):
   if (type(x) == int and type(y) == int):
     return x+y
   return None
-  
+
+# added some   
 # Someone's gonna implement this
 def mult(x,y):
   return x*y
 
-# The best way is just heading directly to the police maybe. 
-# Stokers are not like what you think.
