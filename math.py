f0b9fb32f541f043e8df327f7b64fc4394aea3b2
Jeewon74 <jeewoncn@gmail.com>
2021-08-01 12:21:25 +0900

    new changes number 2

--- math.py
+++ math.py
@@ -10,5 +10,5 @@ def mult(x,y):
   return x*y
 
 # What to do when psycho is hacking you?
-1) call the police
-2) do not respond. Responding makes that person more excited. One of the major characteristic of a psycho.
+# 1) call the police
+# 2) do not respond. Responding makes that person more excited. One of the major characteristic of a psycho.
